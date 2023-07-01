8a4e72926bda25fa507cbf33a491e5b4d1f89fb2
jvpi <jvpi189@gmail.com>
2023-06-30 13:35:00 -1200

    app install commit

--- bundle.js
+++ bundle.js
@@ -1,139 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src;
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) {
-/******/ 					var i = scripts.length - 1;
-/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
-/******/ 				}
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
 
-;// CONCATENATED MODULE: ./src/asset/img/Icono-144-px.png
-const Icono_144_px_namespaceObject = __webpack_require__.p + "images/Icono-144-px.png";
-;// CONCATENATED MODULE: ./src/asset/img/consola.png
-const consola_namespaceObject = __webpack_require__.p + "images/consola.png";
-;// CONCATENATED MODULE: ./src/asset/img/pcgamer.jpg
-const pcgamer_namespaceObject = __webpack_require__.p + "images/pcgamer.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/1.png
-const _1_namespaceObject = __webpack_require__.p + "images/1.png";
-;// CONCATENATED MODULE: ./src/asset/img/5.png
-const _5_namespaceObject = __webpack_require__.p + "images/5.png";
-;// CONCATENATED MODULE: ./src/asset/img/6.png
-const _6_namespaceObject = __webpack_require__.p + "images/6.png";
-;// CONCATENATED MODULE: ./src/asset/img/Anpar.jpg
-const Anpar_namespaceObject = __webpack_require__.p + "images/Anpar.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/asphalt8.jpg
-const asphalt8_namespaceObject = __webpack_require__.p + "images/asphalt8.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/assassin.jpg
-const assassin_namespaceObject = __webpack_require__.p + "images/assassin.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/b.jpg
-const b_namespaceObject = __webpack_require__.p + "images/b.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/battle.jpg
-const battle_namespaceObject = __webpack_require__.p + "images/battle.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/battlefield.jpg
-const battlefield_namespaceObject = __webpack_require__.p + "images/battlefield.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/carrace.jpg
-const carrace_namespaceObject = __webpack_require__.p + "images/carrace.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/circle.png
-const circle_namespaceObject = __webpack_require__.p + "images/circle.png";
-;// CONCATENATED MODULE: ./src/asset/img/coldwar.png
-const coldwar_namespaceObject = __webpack_require__.p + "images/coldwar.png";
-;// CONCATENATED MODULE: ./src/asset/img/controller.jpg
-const controller_namespaceObject = __webpack_require__.p + "images/controller.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/controller1.png
-const controller1_namespaceObject = __webpack_require__.p + "images/controller1.png";
-;// CONCATENATED MODULE: ./src/asset/img/controller5.png
-const controller5_namespaceObject = __webpack_require__.p + "images/controller5.png";
-;// CONCATENATED MODULE: ./src/asset/img/controller6.png
-const controller6_namespaceObject = __webpack_require__.p + "images/controller6.png";
-;// CONCATENATED MODULE: ./src/asset/img/controller7.png
-const controller7_namespaceObject = __webpack_require__.p + "images/controller7.png";
-;// CONCATENATED MODULE: ./src/asset/img/craft.jpeg
-const craft_namespaceObject = __webpack_require__.p + "images/craft.jpeg";
-;// CONCATENATED MODULE: ./src/asset/img/cube.png
-const cube_namespaceObject = __webpack_require__.p + "images/cube.png";
-;// CONCATENATED MODULE: ./src/asset/img/cube2.png
-const cube2_namespaceObject = __webpack_require__.p + "images/cube2.png";
-;// CONCATENATED MODULE: ./src/asset/img/cube3.png
-const cube3_namespaceObject = __webpack_require__.p + "images/cube3.png";
-;// CONCATENATED MODULE: ./src/asset/img/download.jpg
-const download_namespaceObject = __webpack_require__.p + "images/download.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/download__4_-removebg-preview.png
-const download_4_removebg_preview_namespaceObject = __webpack_require__.p + "images/download__4_-removebg-preview.png";
-;// CONCATENATED MODULE: ./src/asset/img/feedback.jpg
-const feedback_namespaceObject = __webpack_require__.p + "images/feedback.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/fifa21.webp
-const fifa21_namespaceObject = __webpack_require__.p + "images/fifa21.webp";
-;// CONCATENATED MODULE: ./src/asset/img/flame.jpg
-const flame_namespaceObject = __webpack_require__.p + "images/flame.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/fortnit.jpg
-const fortnit_namespaceObject = __webpack_require__.p + "images/fortnit.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/fortnite.jpg
-const fortnite_namespaceObject = __webpack_require__.p + "images/fortnite.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/game.jpg
-const game_namespaceObject = __webpack_require__.p + "images/game.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/game2.jpg
-const game2_namespaceObject = __webpack_require__.p + "images/game2.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/game3.jpg
-const game3_namespaceObject = __webpack_require__.p + "images/game3.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/gamebg.jpg
-const gamebg_namespaceObject = __webpack_require__.p + "images/gamebg.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/image9.jpg
-const image9_namespaceObject = __webpack_require__.p + "images/image9.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/incrdible.png
-const incrdible_namespaceObject = __webpack_require__.p + "images/incrdible.png";
-;// CONCATENATED MODULE: ./src/asset/img/jumanji.jpg
-const jumanji_namespaceObject = __webpack_require__.p + "images/jumanji.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/nba.jpg
-const nba_namespaceObject = __webpack_require__.p + "images/nba.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/NEW DESING.png
-const NEW_DESING_namespaceObject = __webpack_require__.p + "images/NEW DESING.png";
-;// CONCATENATED MODULE: ./src/asset/img/pes22.webp
-const pes22_namespaceObject = __webpack_require__.p + "images/pes22.webp";
-;// CONCATENATED MODULE: ./src/asset/img/rakuto.png
-const rakuto_namespaceObject = __webpack_require__.p + "images/rakuto.png";
-;// CONCATENATED MODULE: ./src/asset/img/spider.png
-const spider_namespaceObject = __webpack_require__.p + "images/spider.png";
-;// CONCATENATED MODULE: ./src/asset/img/vaklish.png
-const vaklish_namespaceObject = __webpack_require__.p + "images/vaklish.png";
-;// CONCATENATED MODULE: ./src/asset/img/warxone.jpg
-const warxone_namespaceObject = __webpack_require__.p + "images/warxone.jpg";
-;// CONCATENATED MODULE: ./src/asset/img/wras.jpg
-const wras_namespaceObject = __webpack_require__.p + "images/wras.jpg";
 ;// CONCATENATED MODULE: ./src/js/geolocalizacion/index.js
 /* harmony default export */ function geolocalizacion() {
     getLocation()
@@ -224,7 +92,39 @@ function desplegarOpciones() {
 }
 
 
+;// CONCATENATED MODULE: ./src/js/btn-install/btnInstall.js
+/* harmony default export */ function btnInstall() {
+    installprompt()
+}
+
+let deferredPrompt; // Allows to show the install prompt
+const installButton = document.getElementById("install_button");
+
+function installprompt() {
+    window.addEventListener("beforeinstallprompt", e => {
+        e.preventDefault();
+        deferredPrompt = e;
+        installButton.hidden = false;
+        installButton.addEventListener("click", installApp);
+    });
+}
+
+function installApp() {
+    deferredPrompt.prompt();
+    installButton.disabled = true;
+    deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+            console.log("PWA setup accepted");
+            installButton.hidden = true;
+        } else {
+            console.log("PWA setup rejected");
+        }
+        installButton.disabled = false;
+        deferredPrompt = null;
+    });
+}
 ;// CONCATENATED MODULE: ./src/js/app.js
+
 
 
 
@@ -234,17 +134,18 @@ function desplegarOpciones() {
     modal()
     Selectbox()
     validarsw()
+    btnInstall()
 }
 
 
 
 ;// CONCATENATED MODULE: ./src/index.js
+/*import  './css/style.css'
+import  './css/reset.css'
+import './asset/iconos/style.css'*/
 
 
-
-
-
-                                              
+//import "./asset/img/Icono-144-px.png"; import"./asset/img/consola.png"; import"./asset/img/pcgamer.jpg";  import"./asset/img/1.png"; import"./asset/img/5.png"; import"./asset/img/6.png"; import"./asset/img/Anpar.jpg"; import"./asset/img/asphalt8.jpg"; import"./asset/img/assassin.jpg"; import"./asset/img/b.jpg"; import"./asset/img/battle.jpg"; import"./asset/img/battlefield.jpg"; import"./asset/img/carrace.jpg"; import"./asset/img/circle.png"; import"./asset/img/coldwar.png"; import"./asset/img/controller.jpg"; import"./asset/img/controller1.png"; import"./asset/img/controller5.png"; import"./asset/img/controller6.png"; import"./asset/img/controller7.png"; import"./asset/img/craft.jpeg"; import"./asset/img/cube.png"; import"./asset/img/cube2.png"; import"./asset/img/cube3.png"; import"./asset/img/download.jpg"; import"./asset/img/download__4_-removebg-preview.png"; import"./asset/img/feedback.jpg"; import"./asset/img/fifa21.webp"; import"./asset/img/flame.jpg"; import"./asset/img/fortnit.jpg"; import"./asset/img/fortnite.jpg"; import"./asset/img/game.jpg"; import"./asset/img/game2.jpg"; import"./asset/img/game3.jpg"; import"./asset/img/gamebg.jpg"; import"./asset/img/image9.jpg"; import"./asset/img/incrdible.png"; import"./asset/img/jumanji.jpg"; import"./asset/img/nba.jpg"; import"./asset/img/NEW DESING.png"; import"./asset/img/pes22.webp"; import"./asset/img/rakuto.png"; import"./asset/img/spider.png"; import"./asset/img/vaklish.png"; import"./asset/img/warxone.jpg"; import"./asset/img/wras.jpg";
 
 app()
 
